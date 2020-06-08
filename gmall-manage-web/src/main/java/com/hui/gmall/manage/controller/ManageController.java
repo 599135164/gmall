85fd670f76f4b9b6c15f6fd3822ee3e8d7131ca2
shenhui <599135164@qq.com>
2020-06-08 18:00:36 +0800

    搭建gmall-manage-web工程

---
+++ gmall-manage-web/src/main/java/com/hui/gmall/manage/controller/ManageController.java
@@ -0,0 +1,21 @@
+package com.hui.gmall.manage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @author shenhui
+ * @version 1.0
+ * @date 2020/6/8 17:56
+ */
+@Controller
+public class ManageController {
+//    @RequestMapping("getCatalog1")
+//    @ResponseBody
+//    public List<> getCatalog(){
+//        return "";
+//    }
+}
---
+++ gmall-manage-web/src/test/java/com/hui/gmall/gmallmanageweb/GmallManageWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hui.gmall.gmallmanageweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallManageWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
